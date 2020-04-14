150219b7620618c296c2a0d9d2cc2bc00d532a77
Kyle A. Matheny <1580012+ph-One@users.noreply.github.com>
2020-04-13 21:10:04 -0400

    Errored out when using Clear Linux, use CentOS

--- setup.js
+++ setup.js
@@ -8,7 +8,7 @@ const numNodes = config => config.controlPlane.nodes + config.workers.nodes;
 
 const backend = {
   docker: {
-    image: 'quay.io/footloose/clearlinux',
+    image: 'quay.io/footloose/centos7:0.6.0',
     // The below is required for dockerd to run smoothly.
     // See also: https://github.com/weaveworks/footloose#running-dockerd-in-container-machines
     privileged: true,
